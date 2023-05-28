dc878fcc1fa4dd5e616561b448104bd09cf5aa6e
venkatamaheshwara <entenkivenkatamaheshwara@gmail.com>
2023-05-28 16:25:41 +0530

    edited app.py set the map_location=cpu

--- app.py
+++ app.py
@@ -59,10 +59,8 @@ disease_classes = ['Apple___Apple_scab',
 
 disease_model_path = 'models/plant_disease_model.pth'
 disease_model = ResNet9(3, len(disease_classes))
-# disease_model.load_state_dict(torch.load(
-#     disease_model_path, map_location=torch.device('cpu')))
 disease_model.load_state_dict(torch.load(
-    disease_model_path, map_location=torch.device('cuda')))
+    disease_model_path, map_location=torch.device('cpu')))
 disease_model.eval()
 
 
